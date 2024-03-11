c332b9e148a50a6927c73c6afdb8af5ae2d15e19
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-03-10 22:08:51 -0700

    added navigation bar for the mobile version and added more toggles and also the hamburger menu for the mobile version

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { SocialIcon } from "react-social-icons"
 import {motion} from "framer-motion"
 import useThemeSwitcher from "./hooks/useThemeSwitcher"
 import { GithubIcon, LinkedInIcon, MoonIcon, SunIcon } from "./Icons"
+import { useState } from "react"
 
 const links = [
     {
@@ -26,13 +27,33 @@ const links = [
     }
 ]
 
+const CustomMobileLink = ({href, title, toggle}) => {
+
+    const router = usePathname();
+    const handleClick = () => {
+        toggle()
+        router.push(href)
+    }
+
+    return(
+        <button onClick={handleClick} className="relative group" href={href}>
+            {title}
+            <span className={`h-[2px] inline-block absolute bg-dark dark:text-dark dark:bg-light left-0 bottom-0.5
+            group-hover:w-full transition-[width] ease duration-300 ${router === href ? 'w-full' :' w-0'}`}>
+                &nbsp;</span>
+        </button>
+    )
+}
+
 const CustomLink = ({href, title}) => {
 
     const router = usePathname();
+
+
     return(
         <Link className="relative group" href={href}>
             {title}
-            <span className={`h-[2px] inline-block absolute bg-dark dark:bg-light left-0 bottom-0.5
+            <span className={`h-[2px] inline-block absolute bg-dark dark:text-dark dark:bg-light left-0 bottom-0.5
             group-hover:w-full transition-[width] ease duration-300 ${router === href ? 'w-full' :' w-0'}`}>
                 &nbsp;</span>
         </Link>
@@ -42,40 +63,80 @@ const CustomLink = ({href, title}) => {
 const Navbar = () => {
 
     const[mode, setMode] = useThemeSwitcher();
+    const[open, setOpen] = useState(false)
+
+    const handleClick = () => {
+        setOpen(!open)
+    }
   return (
     <header className="w-full h-full py-8 px-32 dark:bg-dark dark:text-light
-     font-medium flex items-center justify-between sticky">
+     font-medium flex items-center justify-between relative">
 
-        <nav
-         className="flex flex-row gap-6 w-1/3">
-            {/* <CustomLink href='/' title={'Home'}></CustomLink>
-            <CustomLink href={'/about'} title={'About'}></CustomLink>
-            <CustomLink href={'/projects'} title={'Projects'}></CustomLink>
-            <CustomLink href={'/contact'} title={'Contact'}></CustomLink> */}
-            {links.map((link) => (
-                <CustomLink key={link.title} href={link.href} title={link.title}/>
-            ))}
-        </nav>
-        <div className="w-1/3">
-            <Logo/>
+        <button className="flex-col justify-center items-center md:hidden flex" onClick={handleClick}>
+            <span className={`bg-dark dark:bg-light block transition-all ease-out duration-300 h-0.5 w-6 rounded-sm -translate-y-0.5 ${ open ? "translate-y-1 rotate-45" : "-translate-y-0.5"}`}></span>
+            <span className={`bg-dark dark:bg-light block transition-all ease-out duration-300 h-0.5 w-6 rounded-sm my-0.5 ${open ? "opacity-0": "opacity-100"}`}></span>
+            <span className={`bg-dark dark:bg-light block transition-all ease-out duration-300 h-0.5 w-6 rounded-sm -translate-y-0.5 ${ open ? "-translate-y-1 -rotate-45" : "translate-y-0.5"}`}></span>
+        </button>
+
+        <div className="w-full hidden justify-center items-center md:flex">
+            <nav
+            className="flex flex-row gap-6 w-1/3">
+                {/* <CustomLink href='/' title={'Home'}></CustomLink>
+                <CustomLink href={'/about'} title={'About'}></CustomLink>
+                <CustomLink href={'/projects'} title={'Projects'}></CustomLink>
+                <CustomLink href={'/contact'} title={'Contact'}></CustomLink> */}
+                {links.map((link) => (
+                    <CustomLink key={link.title} href={link.href} title={link.title}/>
+                ))}
+            </nav>
+            <div className="w-1/3">
+                <Logo/>
+            </div>
+            
+            <nav className="flex gap-2 w-1/3 justify-center items-center">
+
+                <Link className="w-8 h-8 mr-4" href="https://github.com">
+                    <GithubIcon className="w-full h-full"/>
+                </Link>
+                
+                <Link className="w-8 h-8" href="https://linkedin.com">
+                    <LinkedInIcon className="w-full h-full"/>
+                </Link>
+                
+
+                <button className={`ml-4 flex items-center justify-center rounded-full p-[5px] ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
+                onClick={() => setMode(mode === "light" ? "dark" : "light")}>
+                    {mode === "dark" ? <SunIcon className={"fill-dark"}/> : <MoonIcon className={"fill-dark"}/> }
+                </button>
+            </nav>
         </div>
-        
-        <nav className="flex gap-2 w-1/3 justify-center items-center">
 
-            <Link className="w-8 h-8 mr-4" href="https://github.com">
-                <GithubIcon className="w-full h-full"/>
-            </Link>
+        {open && 
+        <div className="w-[70vw] z-30 flex-col justify-center items-center md:flex fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+        bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
+            <nav
+            className="flex flex-col justify-center items-center gap-6">
+                {/* <CustomLink href='/' title={'Home'}></CustomLink>
+                <CustomLink href={'/about'} title={'About'}></CustomLink>
+                <CustomLink href={'/projects'} title={'Projects'}></CustomLink>
+                <CustomLink href={'/contact'} title={'Contact'}></CustomLink> */}
+                {links.map((link) => (
+                    <CustomMobileLink key={link.title} href={link.href} title={link.title} toggle={handleClick}/>
+                ))}
+            </nav>
             
-            <Link className="w-8 h-8" href="https://linkedin.com">
-                <LinkedInIcon className="w-full h-full"/>
-            </Link>
-            
+            <nav className="flex gap-2 justify-center items-center">
 
-            <button className={`ml-4 flex items-center justify-center rounded-full p-[5px] ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
-             onClick={() => setMode(mode === "light" ? "dark" : "light")}>
-                {mode === "dark" ? <SunIcon className={"fill-dark"}/> : <MoonIcon className={"fill-dark"}/> }
-            </button>
-        </nav>
+                <Link className="w-8 h-8 mr-4" href="https://github.com">
+                    <GithubIcon className="w-full h-full"/>
+                </Link>
+                
+                <Link className="w-8 h-8" href="https://linkedin.com">
+                    <LinkedInIcon className="w-full h-full"/>
+                </Link>
+            
+            </nav>
+        </div>}
         
     </header>
   )
