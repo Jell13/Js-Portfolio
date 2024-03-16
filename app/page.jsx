ac7fd1b841945d3907aa73a004ff7c89eec376dd
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-03-16 11:36:19 -0700

    added the resume link

--- app/page.jsx
+++ app/page.jsx
@@ -30,8 +30,8 @@ export default function Home() {
         </h1>
 
         <div className='mt-10'>
-          <Link href="/" target={"_blank"} className='bg-dark p-2 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark duration-300 border-2 border-solid
-          border-transparent hover:border-dark text-light dark:text-dark dark:bg-light dark:hover:bg-dark dark:hover:text-light' download={true}>Resume</Link>
+          <a href="/Resume-Jason.pdf" className='bg-dark p-2 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark duration-300 border-2 border-solid
+          border-transparent hover:border-dark text-light dark:text-dark dark:bg-light dark:hover:bg-dark dark:hover:text-light' download='Resume-Jason'>Resume</a>
         </div>
       </motion.div>
     </>
