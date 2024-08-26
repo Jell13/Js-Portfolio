e9ec559cf14caba8e8f81bb74afccda992b7585a
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-26 10:50:02 -0700

    changed the format for the projects page and removed the spline components

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import {motion} from 'framer-motion'
 export default function Home() {
   return (
     <>
-      <div className='h-full w-full '>
+      <div className='h-full w-full md:px-32'>
           <motion.div
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
@@ -15,27 +15,29 @@ export default function Home() {
             stiffness: 100,
             damping: 20,
             delay:1.3
-          }} className='h-full w-full flex flex-col justify-center items-center'>
-
-            <div className=" w-24 h-24 flex justify-center items-center relative rounded-full border-2 border-white p-1">
-              <div className="inset-0 opacity-100 absolute w-[100%] h-[105%] bg-gradient-to-r dark:from-blue-500 dark:to-purple-600 from-orange-500 to-yellow-300 rounded-full blur"></div>
-              <img src='/title.png' width={70} height={70} className="rounded-full w-full relative hover:opacity-75 duration-200"/>
+          }} className='h-full w-full flex flex-col gap-5 justify-center items-center'>
+            <div className='flex flex-col justify-center items-center'>
+              <div className=" w-24 h-24 flex justify-center items-center relative rounded-full border-2 border-white p-1">
+                <div className="inset-0 opacity-100 absolute w-[100%] h-[105%] bg-gradient-to-r dark:from-blue-500 dark:to-purple-600 from-orange-500 to-yellow-300 rounded-full blur"></div>
+                <img src='/title.png' width={70} height={70} className="rounded-full w-full relative hover:opacity-75 duration-200"/>
+              </div>
+              <p className='pt-2'>Software Developer</p>
+              <h1 className='text-2xl'>
+                <Typewriter
+                  options={{
+                    strings: ["Jason Sugiharto", 'Data_Guy', 'CodeLover'],
+                    autoStart: true,
+                    loop: true,
+                    delaySpeed: 2000
+                  }}
+                />
+              </h1>
+              <div className='mt-10'>
+                <a href="/Resume-Jason.pdf" className='bg-dark p-2 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark duration-300 border-2 border-solid
+                border-transparent hover:border-dark text-light dark:text-dark dark:bg-light dark:hover:bg-dark dark:hover:text-light' download='Resume-Jason.pdf'>Resume</a>
+              </div>
             </div>
-            <p className='pt-2'>Software Developer</p>
-            <h1 className='text-2xl'>
-              <Typewriter
-                options={{
-                  strings: ["Jason Sugiharto", 'Data_Guy', 'CodeLover'],
-                  autoStart: true,
-                  loop: true,
-                  delaySpeed: 2000
-                }}
-              />
-            </h1>
-            <div className='mt-10'>
-              <a href="/Resume-Jason.pdf" className='bg-dark p-2 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark duration-300 border-2 border-solid
-              border-transparent hover:border-dark text-light dark:text-dark dark:bg-light dark:hover:bg-dark dark:hover:text-light' download='Resume-Jason.pdf'>Resume</a>
-            </div>
+            
           </motion.div>
       </div>
     </>
