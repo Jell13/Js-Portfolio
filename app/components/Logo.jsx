a78a125625f805e6bb2afff040dc7104d71632b7
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-26 11:51:33 -0700

    changed the logo border thickness and color when hovered and also changed the spline component

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -10,12 +10,12 @@ const Logo = () => {
     <div className='flex items-center justify-center mt-2'>
       <MotionLink 
       whileHover={
-        {backgroundColor:["#121212", "#00FFFF","#800080","#FFFF00", "#121212"],
+        {backgroundColor:["#3b82f6", "#f97316"],
         transition:{duration:1, repeat:Infinity}
       }
         
       }
-      className='w-16 h-16 bg-dark text-light border-2 border-solid border-transparent dark:border-light flex items-center justify-center rounded-full text-2xl font-bold hover:bg-blue-950 duration-300' href="/">
+      className='w-16 h-16 bg-dark text-light border-[1px] border-solid border-transparent dark:border-light flex items-center justify-center rounded-full text-2xl font-bold hover:bg-blue-950 duration-300' href="/">
         JS
       </MotionLink>
     </div>
